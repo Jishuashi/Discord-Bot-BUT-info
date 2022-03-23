f131254f764a1f479a98842bb4dbcd930bebb183
Chartier Hugo <72805472+Jishuashi@users.noreply.github.com>
2022-03-23 15:12:36 +0100

    Create SlashCommands

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,7 +1,10 @@
 module.exports = {
     name: 'ready',
     once: true,
-    execute(client) {
+    async execute(client) {
         console.log("Je suis ready");
+
+        const devGuild = await client.guilds.cache.get('952691106712260708');
+        devGuild.commands.set(client.commands.map(cmd => cmd))
     }
 }
