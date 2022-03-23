90ebd2cbfa1d3b148b2a757984ee260ad62026f4
Chartier Hugo <72805472+Jishuashi@users.noreply.github.com>
2022-03-23 11:03:50 +0100

    Create Event Handlers

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const { Client } = require("discord.js");
 const dotenv = require("dotenv");
-
 dotenv.config();
-
 const client = new Client({ intents: 98045 });
 
-client.once('ready', () => {
-    console.log("Je suis prêt");
-});
+require("./utils/handlers/EventUtil")(client);
+
 
 client.login(process.env.DISCORD_TOKEN);
