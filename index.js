5d2006abea69da9fc25a815134eb04540da10c68
Chartier Hugo <72805472+Jishuashi@users.noreply.github.com>
2022-03-23 12:05:50 +0100

    Add log and process

--- index.js
+++ index.js
@@ -7,4 +7,10 @@ client.commands = new Collection();
 
 ['CommandUtil', 'EventUtil'].forEach(handler => { require(`./utils/handlers/${handler}`)(client) })
 
+process.on('exit', code => { console.log(`Le process s'est arrêté avec le code: ${code}`) });
+process.on('uncaughtException', ((err, origin) => { console.log(`UNCAUGTH_EXEPTION ${err}, Origin : ${origin}`) }));
+process.on('unhandledRejection', ((reason, promise) => { console.log(`UNHANDLED_REJECTION: ${reason} \n ------ \n ${promise}`) }));
+process.on('warning', ((...args) => { console.log(...args) }));
+
+
 client.login(process.env.DISCORD_TOKEN);
--- utils/handlers/CommandUtil.js
+++ utils/handlers/CommandUtil.js
@@ -5,7 +5,14 @@ const pGlob = promisify(glob);
 module.exports = async client => {
     (await pGlob(`${process.cwd()}/commands/*/*.js`)).map(async cmdFile => {
         const cmd = require(cmdFile);
+
+        if (!cmd.name) return console.log(`------------------\nCommande non chargé: erreur de typo (ou pas de nom)\n Fichier --> ${cmdFile}\n------------------`)
+
+
         console.log(cmd.name, cmd)
         client.commands.set(cmd.name, cmd);
+
+        console.log(`Commande chargé : ${cmd.name}`);
+
     })
 }
